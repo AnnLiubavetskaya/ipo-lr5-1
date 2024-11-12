9016513f19352c369d1fb28f55f145a4e7fcf241
Lyap <20anita07l@gmail.com>
2024-11-12 14:07:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ all_gls = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"]
 gls = 0
 sgls = 0
 for i in strochka.lower() :
-    if i in all_gls:
-        gls += 1
-    else:
-        sgls +=1
+    if i.isalpha():
+        if i in all_gls:
+            gls += 1
+        else:
+            sgls +=1
 print("Длина вашей строки равна: ", len(strochka))
 print("Гласных в ней: ", gls)
 print("Согласных в ней: ", sgls)
